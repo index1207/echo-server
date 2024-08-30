5631421c1302bd00a7a4c48a915f3b53ebb19d49
index <dev.index@outlook.com>
2024-08-30 14:51:31 +0900

    initialize

---
+++ src/core/Session.cpp
@@ -0,0 +1,41 @@
+#include "Session.hpp"
+
+#include <fmt/base.h>
+#include <net/context.hpp>
+
+Session::Session() : _buffer {0,} {}
+Session::~Session() = default;
+
+void Session::Run(std::unique_ptr<net::socket>&& sock)
+{
+    _thisPtr = shared_from_this();
+    _sock = std::move(sock);
+
+    auto ctx = new net::context;
+    ctx->buffer = _buffer;
+    ctx->completed = std::bind(&Session::OnReceiveCompleted, this, std::placeholders::_1, std::placeholders::_2);
+    if (!_sock->receive(ctx))
+        OnReceiveCompleted(ctx, false);
+
+    OnConnected(_sock->get_remote_endpoint().value());
+}
+
+void Session::OnReceiveCompleted(net::context* ctx, bool success)
+{
+    if (ctx->length == 0)
+    {
+        OnDisconnected(_sock->get_remote_endpoint().value());
+        _thisPtr = nullptr;
+    }
+    else if (success)
+    {
+        OnReceived(ctx->buffer, ctx->length);
+        if (!_sock->receive(ctx))
+            OnReceiveCompleted(ctx, false);
+    }
+}
+
+void Session::OnSendCompleted(net::context* ctx, bool)
+{
+
+}
